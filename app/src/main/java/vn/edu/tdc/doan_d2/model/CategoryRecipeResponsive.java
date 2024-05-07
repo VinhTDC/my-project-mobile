14b47243e1defe9bdee37ff20bc66f4bbadd3701
VinhTDC <22211TT4921@mail.tdc.edu.vn>
2024-05-07 21:04:21 +0700

    save-data-in-firebase

--- app/src/main/java/vn/edu/tdc/doan_d2/model/CategoryRecipeResponsive.java
+++ app/src/main/java/vn/edu/tdc/doan_d2/model/CategoryRecipeResponsive.java
@@ -5,13 +5,19 @@ import android.util.Log;
 
 import androidx.lifecycle.MutableLiveData;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import vn.edu.tdc.doan_d2.R;
 import vn.edu.tdc.doan_d2.model.category.Categories;
+import vn.edu.tdc.doan_d2.model.category.Category;
 import vn.edu.tdc.doan_d2.model.category.RecipeCategory;
 import vn.edu.tdc.doan_d2.serviceapi.RecipeCategoryApiService;
 import vn.edu.tdc.doan_d2.serviceapi.RetrofitInstance;
@@ -41,6 +47,7 @@ public class CategoryRecipeResponsive {
                     data = (ArrayList<String>) categories.getData();
                     dataMutableLiveData.setValue(data);
                     Log.d("API_Response", "Data: " + data.toString());
+                    saveCategoriesToFirebase(categories.getData());
                 }else {
                     Log.e("API_Response", "Failed to get data from API. Error code: " + response.code());
                 }
@@ -54,4 +61,31 @@ public class CategoryRecipeResponsive {
         });
         return dataMutableLiveData;
     }
+    private void saveCategoriesToFirebase(List<String> categoryNames) {
+        if (categoryNames != null) {
+            DatabaseReference databaseRef = FirebaseDatabase.getInstance().getReference().child("categories");
+            for (String categoryName : categoryNames) {
+                DatabaseReference categoryRef = databaseRef.push();
+                String categoryId = categoryRef.getKey(); // Lấy id tự động từ Firebase
+
+                // Tải lên ảnh và lấy đường dẫn ảnh từ Firebase Storage
+                String imageUrl = uploadImageToFirebaseStorage(categoryName);
+
+                // Tạo một đối tượng Category mới với id, tên và imageUrl
+                Category newCategory = new Category(categoryId, categoryName, imageUrl);
+
+                // Lưu đối tượng Category vào Firebase Realtime Database
+                categoryRef.setValue(newCategory);
+            }
+        }
+    }
+    private String uploadImageToFirebaseStorage(String categoryName) {
+        // Triển khai logic tải lên ảnh và lấy đường dẫn ảnh từ Firebase Storage tại đây
+        // Ví dụ, bạn có thể tải lên ảnh với tên là "category_" + categoryName + ".jpg"
+        // và trả về đường dẫn ảnh tương ứng
+
+        // Đoạn mã này chỉ để minh họa, bạn cần tự triển khai logic tải lên ảnh và lấy đường dẫn
+        return "https://example.com/images/category_" + categoryName + ".jpg";
+    }
+
 }
