86a08806b1baa35cfd4b8a4f0b7b2a115d11221e
ThanhTa2ngan4 <tahynhatthanh2004@gmail.com>
2024-05-30 21:52:00 +0700

    Hiển thị logo màn hình chờ + fig bug

---
+++ app/src/main/java/vn/edu/tdc/doan_d2/SplashActivity.java
@@ -0,0 +1,26 @@
+package vn.edu.tdc.doan_d2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT =3000; // 3 giây
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_layout);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
